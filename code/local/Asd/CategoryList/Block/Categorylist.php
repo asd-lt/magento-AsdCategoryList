2512c4122a0ead8641efc645498eb99edb7ff855
Simonas Dimavičius <asd@asd.lt>
2014-05-06 10:27:00 +0300

    First Stable Build

---
+++ code/local/Asd/CategoryList/Block/Categorylist.php
@@ -0,0 +1,40 @@
+<?php
+
+class Asd_CategoryList_Block_Categorylist extends \Mage_Core_Block_Abstract implements Mage_Widget_Block_Interface {
+
+    protected function _toHtml() {
+        $html = '';
+        $currentCategory = Mage::getModel('catalog/layer')->getCurrentCategory();
+        $currentCategoryLevel = $currentCategory->getLevel();
+        if( $currentCategoryLevel == 2 ) {
+            $currentCategoryId = $currentCategory->getId();
+        } elseif( $currentCategoryLevel > 2 ) {
+            $currentCategoryId = $currentCategory->getParentId();
+        } else {
+            $currentCategoryId = 0;
+        }
+
+        if( $currentCategoryId > 0 ) {
+            $parentCategory = Mage::getModel('catalog/category')->load( $currentCategoryId );
+            $childCategories = $parentCategory->getChildrenCategories()->addIsActiveFilter();
+            if( $childCategories->count() > 0 ) {
+                $html .= '<nav><ul>';
+                foreach( $childCategories as $category ) {
+                    $class = $currentCategoryId == $category->getId() ? ' class="active"' : '';
+                    $html .= '<li' . $class . '><a href="' . $category->getUrl() . '">' . $category->getName() . '</a></li>';
+                }
+                $html .= '</ul></nav>';
+            }
+        }
+        return $html;
+    }
+    
+    public function addData(array $arr) {
+        
+    }
+
+    public function setData($key, $value = null) {
+        
+    }
+
+}
---
+++ code/local/Asd/CategoryList/Helper/Data.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Category List Widget Helper
+ */
+class Asd_CategoryList_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    
+}
